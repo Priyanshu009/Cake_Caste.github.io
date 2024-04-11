43f941fb3bbed508186ec708aee70535d952f2a9
Priyanshu009 <priyanshu.parmar02@gmail.com>
2024-04-11 16:07:19 +0530

    cake 189,190,191 added and clay component in js added

---
+++ allSampleRate.js
@@ -0,0 +1,15 @@
+let XS=200;
+let S=250;
+let M=500;
+let L=750;
+let XL=1000;
+
+let car=10;
+let color=20;
+let fruits=30;
+let tag=50;
+let eatabletag=100;
+let chocolate=100;
+let photo=100;
+let doll=100;
+let clay=100;
